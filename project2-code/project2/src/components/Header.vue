9b8db0fcda98de72dd34c8e5a323e547f4c12824
Jana Shyti <jshyti@eagles.bridgewater.edu>
2025-03-26 22:42:01 -0400

    03/26

---
+++ project2-code/project2/src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+    //import Logo from './Logo.vue'
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <Logo />
+            <slot></slot>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+:slotted(*) {
+    font-size: var(--fs-600);
+    padding: 10px;
+}
+
+</style>
---
+++ project2-code/project2/src/views/Home.vue
@@ -0,0 +1,49 @@
+<script setup>
+import Header from '../components/Header.vue'
+</script>
+
+<template>
+    <Header>
+        <nav>
+            <RouterLink to="/join">Join</RouterLink>
+            <RouterLink to="/signin">Sign In</RouterLink>
+        </nav>
+    </Header>
+
+    <main class="background ">
+        <section class="container center vertical">
+        </section>
+    </main>
+</template>
+
+<style scoped>
+
+:deep(a) {
+    text-decoration: none;
+}
+
+html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden; 
+}
+
+
+.background {
+    background-size: cover; 
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100vh; 
+    width: 100vw; 
+    position: fixed; 
+    top: 0;
+    left: 0;
+    z-index: -1; 
+}
+
+</style>
+
+
+
